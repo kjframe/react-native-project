7e15750a97002c0dc86932b089f60c0fc26bab2f
Kyle Frame <kjframe1223@gmail.com>
2022-06-20 20:46:49 -0500

    Added Protoss units/structure images

---
+++ shared/races.js
@@ -0,0 +1,26 @@
+export const RACES = [
+    {
+        id: 0,
+        name: "Protoss",
+        image: require("../assets/images/Protoss_header.webp"),
+        home: "Aiur",
+        featured: false,
+        description: "Aiur is the Protoss homeworld, located on the galaxy's fringe in a star system with a single yellow star and possessing a single moon not unlike Luna."
+    },
+    {
+        id: 1,
+        name:"Terran",
+        image: require("../assets/images/Terran_header.webp"),
+        home: "Tarsonis, Moria, Umoja",
+        featured: false,
+        description: "The terrans arrived in the Koprulu sector via supercarriers in 2259, landing on three separate planets – Tarsonis, Moria and Umoja. They were cut off from Earth."
+    },
+    {
+        id: 2,
+        name:"Zerg",
+        image: require("../assets/images/Zerg_header.webp"),
+        elevation: 2901,
+        featured: false,
+        description: "Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground."
+    }
+];
